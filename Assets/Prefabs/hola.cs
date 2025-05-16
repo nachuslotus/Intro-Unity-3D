9101f3905c7ee53ec734ca98309d359ba0b2d309
nachuslotus <166507478+nachuslotus@users.noreply.github.com>
2025-05-16 11:57:16 -0300

    Initial commit

---
+++ Assets/Prefabs/hola.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hola : MonoBehaviour
+{
+
+    public GameObject CubePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ClonarCubos()
+    {
+        Instantiate(CubePrefab);
+    }
+}
